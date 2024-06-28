98dea0ad52e566d3b935322bdb9aad18f4f044ff
gorillaglass1 <155641361+gorillaglass1@users.noreply.github.com>
2024-06-28 23:15:02 +0900

    Update file2.c

--- chap_10/file2.c
+++ chap_10/file2.c
@@ -4,7 +4,7 @@
 
 int main(void) {
     char line[MAX];
-    FILE * file = fopen("c:\\Users\\hjseo\\Documents\\C\\chap_10\\assets\\test1.txt", "rb");
+    FILE * file = fopen("c:\\Users\\user\\Documents\\C\\chap_10\\assets\\test1.txt", "rb");
     if (file == NULL) {
         printf("파일 열기 실패\n");
         return 1;
